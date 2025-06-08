1a99e2eb704d03a67c86715afe30f23860149c5f
Eri Higuchi <209428143+ehcy223@users.noreply.github.com>
2025-06-08 13:09:11 +0000

    予約フォームCまで

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,8 +3,20 @@ class ReservationsController < ApplicationController
   end
 
   def new
+    @reservation = Reservation.new
   end
 
   def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      redirect_to root_path, notice: "予約が完了しました！"
+    else
+      render :new
+    end
   end
+    private
+
+    def reservation_params
+      params.require(:reservation).permit(:name, :phone, :reserved_at)
+    end
 end
