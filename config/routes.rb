e56db0393d63c4eb86231db4b1569c7d551c90f1
Eri Higuchi <209428143+ehcy223@users.noreply.github.com>
2025-06-08 09:48:06 +0000

    ルーティングまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'reservations/index'
-  get 'reservations/new'
-  get 'reservations/create'
   get 'homes/top'
   root to: 'homes#top'
+  resources :reservations, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
