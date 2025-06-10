1ad7af33d997a5a9b0e57bb9da0c66dd1b15817c
Eri Higuchi <209428143+ehcy223@users.noreply.github.com>
2025-06-10 04:30:26 +0000

    ステップ予約１まで

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -14,6 +14,22 @@ class ReservationsController < ApplicationController
       render :new
     end
   end
+
+  def step1
+    @menu = ["歯が痛い", "詰め物が取れた", "矯正したい", "その他"]
+  end
+
+  def step1_submit
+    selected_menu = params[:menu]
+    if selected_menu.blank?
+      flash[:alert] = "診療メニューを選択してください"
+      redirect_to reservations_step1_path
+      else
+      session[:menu] = selected_menu
+      redirect_to reservations_step2_path
+    end
+  end
+
     private
 
     def reservation_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get 'homes/top'
   root to: 'homes#top'
   resources :reservations, only: [:index, :new, :create]
+  get "reservations/new", to: "reservations#new"
+  get 'reservations/step1', to: 'reservations#step1'
+  post 'reservations/step1', to: 'reservations#step1_submit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
